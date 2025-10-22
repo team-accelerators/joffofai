33c44209455f7c98a684b33f785866ec735e97b4
catherine-chioma <chi.ihuaku@gmail.com>
2025-10-22 04:02:31 -0700

    chore: update dependencies and configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
+  },
+  assetsInclude: ['**/*.png'],
+  resolve: {
+    alias: {
+      './images/': '/public/images/'
+    }
   }
 })
